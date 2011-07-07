fc131c5ba2df57f943fc80f19eec766b7f49e0cc
Josh McArthur <joshua.mcarthur@gmail.com>
2011-07-07 15:49:49 +1200

    Use PostgreSQL, not MySQL. Also don't install Rails

--- railsready.sh
+++ railsready.sh
@@ -61,11 +61,11 @@ echo " * An updated system"
 echo " * Ruby $ruby_version_string"
 echo " * Imagemagick"
 echo " * libs needed to run Rails (sqlite, mysql, etc)"
-echo " * Bundler, Passenger, and Rails gems"
+echo " * Bundler, Passenger"
 echo " * Git"
 
 echo -e "\nThis script is always changing."
-echo "Make sure you got it from https://github.com/joshfng/railsready"
+echo "Make sure you got it from https://github.com/3months/railsready"
 
 # Check if the user has sudo privileges.
 sudo -v >/dev/null 2>&1 || { echo $script_runner has no sudo privileges ; exit 1; }
@@ -99,7 +99,7 @@ echo "==> done..."
 
 echo -e "\n=> Downloading and running recipe for $distro...\n"
 #Download the distro specific recipe and run it, passing along all the variables as args
-wget --no-check-certificate -O $railsready_path/src/$distro.sh https://github.com/joshfng/railsready/raw/master/recipes/$distro.sh && cd $railsready_path/src && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $railsready_path $log_file
+wget --no-check-certificate -O $railsready_path/src/$distro.sh https://github.com/3months/railsready/raw/master/recipes/$distro.sh && cd $railsready_path/src && bash $distro.sh $ruby_version $ruby_version_string $ruby_source_url $ruby_source_tar_name $ruby_source_dir_name $whichRuby $railsready_path $log_file
 echo -e "\n==> done running $distro specific commands..."
 
 #now that all the distro specific packages are installed lets get Ruby
@@ -159,11 +159,11 @@ elif [ $whichRuby -eq 2 ] ; then
 fi
 echo "==> done..."
 
-echo -e "\n=> Installing Bundler, Passenger and Rails..."
+echo -e "\n=> Installing Bundler and Passenger..."
 if [ $whichRuby -eq 1 ] ; then
-  sudo gem install bundler passenger rails --no-ri --no-rdoc >> $log_file 2>&1
+  sudo gem install bundler passenger --no-ri --no-rdoc >> $log_file 2>&1
 elif [ $whichRuby -eq 2 ] ; then
-  gem install bundler passenger rails --no-ri --no-rdoc >> $log_file 2>&1
+  gem install bundler passenger --no-ri --no-rdoc >> $log_file 2>&1
 fi
 echo "==> done..."
 
